4c743459932a2eff89ac186eed355299058e171c
cllepri <cllepri@hotmail.com>
2024-01-16 15:04:09 -0300

    Acerto do Component Categorias de forma automatizada

--- src/App.js
+++ src/App.js
@@ -3,21 +3,7 @@ import Header from "./Components/Header";
 import Banner from "./Components/Banner";
 import Container from "./Components/Container";
 import Card from "./Components/Card";
-import videos from "./Json/videos.json"
-import Category from "./Components/Category";
-
-const categories = [
-
-"Geografia",
-"Como fazer e usar",
-"Astronomia e Geografia",
-"Climatologia, Meteorologia, Vegetação",
-"Geologia e Hidroigrafia"
-]
-
-function filterCategory(id){
-  return videos.filter(video => video.category===categories[id] )}
-
+import Category,{categories,filterCategory}from "./Components/Category";
 
 function App() {
   return (
@@ -25,28 +11,13 @@ function App() {
       <Header/>
       <Banner image= "home" />
       <Container>
-          
-           <Category category={categories[0]}> 
-          {filterCategory(0).map((video)=> <Card url={video.url} cover={video.cover} key={video.id}/>)}
-           </Category>
-           
-           <Category category={categories[1]}> 
-          {filterCategory(1).map((video)=> <Card url={video.url} cover={video.cover} key={video.id}/>)}
-           </Category>
 
-           <Category category={categories[2]}>
+       {categories.map((category, index) =>
+        <Category category={category}> 
+            {filterCategory(index).map((video)=> <Card url={video.url} cover={video.cover} key={video.id}/>)}
+             </Category>
 
-          {filterCategory(2).map((video)=> <Card url={video.url} cover={video.cover} key={video.id}/>)}
-           </Category>
-
-           <Category category={categories[3]}> 
-          {filterCategory(3).map((video)=> <Card url={video.url} cover={video.cover} key={video.id}/>)}
-           </Category>
-
-           <Category category={categories[4]}>
-
-          {filterCategory(4).map((video)=> <Card url={video.url} cover={video.cover} key={video.id}/>)}
-           </Category>
+       )}
 
       </Container> 
       <Footer/>
--- src/Components/Category/index.jsx
+++ src/Components/Category/index.jsx
@@ -1,10 +1,25 @@
 import styles from './Category.module.css'
+import videos from '../../Json/videos.json'
 
-<Category></Category>
+export const categories = 
+[
+    "Geografia",
+    "Como fazer e usar",
+    "Astronomia e Geografia",
+    "Climatologia, Meteorologia, Vegetação",
+    "Geologia e Hidroigrafia"
+]
+    
+export  function filterCategory(id){
+      return videos.filter(video => video.category===categories[id] )}
+
+<Category>
+
+</Category>
 function Category({category, children}){
     return(
         <section className={styles.category}>
-            <h2>Categoria</h2>
+            <h2>{category}</h2>
             <div >
                {children}
             </div>
